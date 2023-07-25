51e49e286500fb72a7639dee04794052901bc199
feng <690174435@qq.com>
2023-07-25 08:24:08 +0800

    Doing(test): 1. Add Content

--- internal/operator/go.mod
+++ internal/operator/go.mod
@@ -20,6 +20,7 @@ require (
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/carlmjohnson/requests v0.23.4 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
--- internal/operator/internal/config_server/envtest/api.go
+++ internal/operator/internal/config_server/envtest/api.go
@@ -10,7 +10,15 @@
 package envtest
 
 // 依赖默认的 Test 账号，请勿删除
+// 依赖默认的项目组
+// 没有以下数据，测试将无法进行。
 const (
+	TestAPIURL     = "http://127.0.0.1:8108/sail/v1"
+	TestCheckHeath = "/system/health"
+
+	TestCreateTestData = "/create_test_data"
+	TestCleanTestData  = "/clean_test_data"
+
 	TestAccountName    = "Test"
 	TestAccountPass    = "Test123"
 	TestProjectGroupId = 1
@@ -20,7 +28,7 @@ type ApiEnvTest struct {
 }
 
 func (*ApiEnvTest) Start() {
-
+	// 检查接口
 }
 
 func (*ApiEnvTest) Stop() {
