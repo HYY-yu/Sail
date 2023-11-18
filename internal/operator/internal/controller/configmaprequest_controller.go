5705b4dc4eb1aa22c7650e13322818b0f819e5ee
feng <690174435@qq.com>
2023-11-18 23:42:00 +0800

    fix: 1. rbac configmaps

--- internal/operator/internal/controller/configmaprequest_controller.go
+++ internal/operator/internal/controller/configmaprequest_controller.go
@@ -44,7 +44,8 @@ type ConfigMapRequestReconciler struct {
 //+kubebuilder:rbac:groups=cmr.sail.hyy-yu.space,resources=configmaprequests,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=cmr.sail.hyy-yu.space,resources=configmaprequests/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=cmr.sail.hyy-yu.space,resources=configmaprequests/finalizers,verbs=update
-//+kubebuilder:rbac:groups="",resources=secret,verbs=get;list
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list
+//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
